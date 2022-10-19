c40b566d26f78d291b95a1cb9df6660caa813562
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-10-19 18:58:59 +0100

    progress made: api called

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import WelcomePage from './components/WelcomePage';
 import yellowBlob from './assets/yellowBlob.svg';
 import blueBlob from './assets/blueBlob.svg';
 function App() {
+  const getTrivialApi = async function () {
+    const res = await fetch('https://opentdb.com/api.php?amount=7&category=9');
+
+    const data = await res.json();
+
+    const values = data.results;
+
+    const questions = values.map((value) => value.question);
+
+    console.log(questions);
+  };
+
+  getTrivialApi();
   return (
     <div className="App">
       <img src={yellowBlob} alt="yellowBlob-img" className="yellowBlob" />
