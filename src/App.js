56114632b80630755edd106138820a5158ac8efe
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-10-20 01:55:52 +0100

    another feat reached

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ import { fetchQuizQuestions } from './api';
 import Questions from './components/Questions';
 const App = () => {
   const [questions, setQuestions] = useState([]);
+  const [startQuiz, setStartQuiz] = useState(false);
   const difficulty = {
     easy: 'easy',
     medium: 'medium',
     hard: 'hard',
+  };
+
+  const handleClick = () => {
+    setStartQuiz((prevState) => !prevState);
+    console.log('Quiz started!');
   };
 
   console.log(fetchQuizQuestions(7, difficulty.easy));
@@ -20,14 +26,19 @@ const App = () => {
   return (
     <div className="App">
       <img src={yellowBlob} alt="yellowBlob-img" className="yellowBlob" />
-      <div className="app__welcomePage">
-        <h1>Quizzical App</h1>
-        <p>Take a test of 7 question. Test your knowledge capacity.</p>
-        <button className="custom__button">
-          Start quiz <IoRocketOutline />
-        </button>
-      </div>
-      <Questions question="How are you today?" />
+      {/* WELCOME PAGE */}
+
+      {startQuiz ? (
+        <Questions question="How are you today?" />
+      ) : (
+        <div className="app__welcomePage">
+          <h1>Quizzical App</h1>
+          <p>Take a test of 7 question. Test your knowledge capacity.</p>
+          <button className="custom__button" onClick={handleClick}>
+            Start quiz <IoRocketOutline />
+          </button>
+        </div>
+      )}
 
       <img src={blueBlob} alt="blueBlob-img" className="blueBlob" />
     </div>
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,6 +4,7 @@ const Questions = ({ question, answer, callback, userAnswer }) => {
   return (
     <div className="app__questions">
       <p className="app__question">{question}</p>
+      {/* <p dangerouslySetInnerHTML={{ question }} /> */}
     </div>
   );
 };
